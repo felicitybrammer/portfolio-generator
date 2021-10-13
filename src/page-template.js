8ad9efb9aa7c9de518737c210d49d0337b2e5eac
Felicity Brammer <felicitybrammer@gmail.com>
2021-10-13 15:52:32 -0700

    copied style sheet

--- src/page-template.js
+++ src/page-template.js
@@ -5,7 +5,7 @@ const generateAbout = aboutText => {
   
   return `
     <section class="my-3" id="about">
-      <h2 class="text-darl bg-primary p-2 display-inline-block>About Me</h2>
+      <h2 class="text-darl bg-primary p-2 display-inline-block">About Me</h2>
       <p>${aboutText}</p>
     </section>
   `;
@@ -76,7 +76,7 @@ module.exports = templateData => {
     <body>
       <header>
         <div class="container flex-row justify-space-between align-center py-3">
-          <h1 class="page-title text-secondary bg-dark py-2 px-3>${header.name}</h1>
+          <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
           <nav class="flex-row">  
             <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">Github</a>
           </nav>
